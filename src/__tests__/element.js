7036b278213a7d94c415202b9b80ff9b7776b6fa
Dcw0818 <60767284+Celscius@users.noreply.github.com>
2026-01-13 09:19:16 +0700

    Update element.js

--- src/__tests__/element.js
+++ src/__tests__/element.js
@@ -24,7 +24,7 @@ describe('create html elment', function() {
   });
 
   it("should add image url to element with id 'img' ", async () => {
-    const url = "https://i.discogs.com/U75yhtLq9MYSvtdqPYEWwvA7MwBHJDiKuHPycpnIOhw/rs:fit/g:sm/q:90/h:337/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTQ5NDE4/MjgtMTQ1ODAxNzQ3/OS05OTQ5LmpwZWc.jpeg"
+    const url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLZDp9FHrjfeJUMVQUOuLss5bUzT0QGWcaZA&s"
     const image = await addImage(url);
     expect(image.id).toBe('img');
 
