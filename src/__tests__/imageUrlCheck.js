f7ec479c7406542df4fd8e184ad764af60d9e712
Dcw0818 <60767284+Celscius@users.noreply.github.com>
2026-01-13 09:18:35 +0700

    Update imageUrlCheck.js

--- src/__tests__/imageUrlCheck.js
+++ src/__tests__/imageUrlCheck.js
@@ -4,7 +4,7 @@ import { checkImageHeader } from '../imageUrlCheck.js'
 describe('function to validate image url', function() {
   
   it('should success because the url is working and contain image', async () => {
-    const url = "https://i.discogs.com/U75yhtLq9MYSvtdqPYEWwvA7MwBHJDiKuHPycpnIOhw/rs:fit/g:sm/q:90/h:337/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTQ5NDE4/MjgtMTQ1ODAxNzQ3/OS05OTQ5LmpwZWc.jpeg"
+    const url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLZDp9FHrjfeJUMVQUOuLss5bUzT0QGWcaZA&s"
     const imageUrl = await checkImageHeader(url)
     expect(imageUrl).toEqual(url);
   });
