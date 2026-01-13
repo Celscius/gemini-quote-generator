f98f428aded547d2bfadc7ee3469580e44dca12f
Dcw0818 <60767284+Celscius@users.noreply.github.com>
2026-01-13 11:22:59 +0700

    Refactor getQuote to accept geminiConfig parameter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import geminiConfig from '../config.js';
-
 import { generateQuote } from './gemini-context.js'
 import { request } from './request.js'
 
-export async function getQuote() {
+export async function getQuote(geminiConfig = {}) {
    try {
       const subject = geminiConfig.SUBJECT ?? "2026"
       const data = await request(geminiConfig, generateQuote(subject));
@@ -13,7 +11,3 @@ export async function getQuote() {
       return { subject: "gemini", title: "oops, something goes wrong :(", quote: err, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTANHa63Y8tpuLLwa9J7yjlm8nAvj01OPQGvQ&s" }
    }
 } 
- 
- 
- 
-
