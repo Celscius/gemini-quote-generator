75a9180b9a7b1c9db46e4c3ca966623de23ac7ed
Dcw0818 <60767284+Celscius@users.noreply.github.com>
2026-01-13 11:21:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 import { getQuote } from './src/index.js'
 import renderElement  from './src/element.js'
 import { hideLoading, displayLoading } from './src/loading.js'
+import { fileExists } from './src/checkFileExists.js'
 
 // Async IIFE
 (async () => {
   try{
     displayLoading()
 
-    const data = await getQuote()
+    const configPath = './config.js';
+    if (!(await fileExists(configPath))) { throw new Error("config file is missing, check the github repo how to create it") }
+
+    const module = await import(configPath);
+    const config = module.default;
+
+    const data = await getQuote(config)
     const elementId = "app"
     
     renderElement(data, elementId)
     hideLoading()
 
   } catch(err) {
-    console.log(err)
+    hideLoading()
+    const data = { subject: "gemini", title: "oops, something goes wrong :(", quote: err, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTANHa63Y8tpuLLwa9J7yjlm8nAvj01OPQGvQ&s" }
+    renderElement(data, 'app')
   }
 })();
 
